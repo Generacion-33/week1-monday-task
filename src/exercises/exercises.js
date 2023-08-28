f242b11941331cac3d4dc6c1120770acbacff030
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-27 22:58:14 -0300

    advances

---
+++ src/exercises/exercises.js
@@ -0,0 +1,77 @@
+/*
+Ejercicio 1:
+1. Declarar dos variables: nombre y usuario sin asignarle ningún valor.
+2. Asignar tu nombre a la variable nombre.
+3. El valor de la variable nombre, debe ser igual a la variable usuario.
+*/
+
+//resolver aquí
+
+/*
+Ejercicio 2:
+¿Cómo nombrarías a dicha variable? *en ingles
+1. Crea una variable con el nombre de "nuestro planeta" usando camelCase. Solo la debes declarar y no asignar.
+2. Crea una variable llamada name.Solo la debes declarar y no asignar.
+*/
+
+//resolver aquí
+
+/*
+Ejercicio 3:
+1. Crea la variable userName usando const, asignandole tu nombre.
+2. Crea otra variable llamada lastName usando const, y asiganale tu apellido.
+*/
+
+//resolver aquí
+
+/*
+ Ejercicio #1
+ Las siguientes declaraciones de variables pueden estar correctas o incorrectas.
+ Tu labor es colocar un true o false si la declaración es correcta o incorrecta respectivamente.
+ Reemplaza null por el valor correspondiente para cada declaración.
+ */
+
+// ¿Es correcto?: let 1erTema
+const exercise4 = true && false
+
+// ¿Es correcto?: const = nombreDeUsuario
+const exercise5 = true && false
+
+// ¿Es correcto?: const $mi_variable$
+const exercise6 = true && false
+
+// ¿Es correcto?: letEdad
+const exercise7 = true && false
+
+// ¿Es correcto?: const class
+const exercise8 = true && false
+
+// ¿Es correcto?: let _nombre
+const exercise9 = true && false
+
+// ¿Es correcto?: let %_nombre
+const exercise10 = true && false
+/*
+
+¿Cuál es la diferencia entre var, let y const?
+*/
+
+// NO TOCAR ESTE CÓDIGO
+module.exports = {
+  //1
+  nombre,
+  usuario,
+  //2
+  ourPlanet,
+  name,
+  //3
+  userName,
+  lastName,
+  exercise4,
+  exercise5,
+  exercise6,
+  exercise7,
+  exercise8,
+  exercise9,
+  exercise10
+}
---
+++ src/tests/exercises.test.js
@@ -0,0 +1,49 @@
+const { nombre, usuario, ourPlanet, name, userName, lastName, exercise4, exercise5, exercise6, exercise7, exercise8, exercise9, exercise10 } = require("../exercises/exercises")
+
+test("exercise1 to be denfined, and user to be denfined", () => {
+  expect(nombre).toBeDefined()
+  expect(usuario).toBeDefined()
+  expect(usuario).toBe(nombre)
+})
+
+test("exercise2 to be undenfined", () => {
+  expect(ourPlanet).toBeUndefined()
+  expect(name).toBeUndefined()
+})
+
+test("exercise3 to be denfined", () => {
+  expect(userName).toBeDefined()
+  expect(lastName).toBeDefined()
+})
+
+test("exercise4 to be false", () => {
+  expect(exercise4).toBe(false)
+})
+
+test("exercise5 to be false", () => {
+  expect(exercise5).toBe(false)
+})
+
+test("exercise6 to be true", () => {
+  expect(exercise6).toBe(true)
+})
+
+test("exercise7 to be false", () => {
+  expect(exercise7).toBe(false)
+})
+
+test("exercise8 to be false", () => {
+  expect(exercise8).toBe(false)
+})
+
+test("exercise9 to be true", () => {
+  expect(exercise9).toBe(true)
+})
+
+test("exercise10 to be false", () => {
+  expect(exercise10).toBe(false)
+})
+
+
+
+
